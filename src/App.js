e2c1937abe96be822c143d23730205b4713e9ad7
Yasmine <y.hartung@gmail.com>
2019-10-04 13:18:17 -0400

    landing page component started

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/landingPage.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+       <LandingPage />
         <a
           className="App-link"
           href="https://reactjs.org"
